16b0bc27622039ab75fa0e85884f496206e0667e
Daria Aleksandrova <106164543+AleksandrovaDaria@users.noreply.github.com>
2023-04-10 23:34:48 +0200

    add  "Question has been deleted" message

--- messages.py
+++ messages.py
@@ -3,4 +3,5 @@ messages = {
     "added_answer": "Answer has been added",
     "delete_answer": "Answer has been deleted",
     "cant_delete": "You can't delete this",
+    "delete_question": "Question has been deleted",
 }
