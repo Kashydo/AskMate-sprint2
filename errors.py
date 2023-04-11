5336eca60c0ec64878584321ef86bafd3d99cb7a
Daria Aleksandrova <106164543+AleksandrovaDaria@users.noreply.github.com>
2023-04-11 17:01:10 +0200

    add not found errors message

--- errors.py
+++ errors.py
@@ -1,5 +1,6 @@
 errors = {
 "empty_title": "Title is empty",
 "empty_message": "Message is empty",
-"wrong_file_extension": "Wrong file type"
+"wrong_file_extension": "Wrong file type",
+"not found": "Not found"
 }
